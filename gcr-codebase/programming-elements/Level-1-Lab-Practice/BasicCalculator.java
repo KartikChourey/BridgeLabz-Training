36e26f3b52e4f16291e5f241385c42f775365b6d
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-17 16:37:09 +0530

    [Kartik Chourey] : Added Program for Basic Calculator

---
+++ gcr-codebase/programming-elements/Level-1-Lab-Practice/BasicCalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+
+        // Scanner object for input
+        Scanner input = new Scanner(System.in);
+
+        // Taking first number
+        System.out.print("Enter first number: ");
+        double number1 = input.nextDouble();
+
+        // Taking second number
+        System.out.print("Enter second number: ");
+        double number2 = input.nextDouble();
+
+        // Performing arithmetic operations
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = number1 / number2;
+
+        // Displaying results
+        System.out.println(
+            "The addition, subtraction, multiplication and division value of 2 numbers " +
+            number1 + " and " + number2 + " is " +
+            addition + ", " + subtraction + ", " +
+            multiplication + ", and " + division
+        );
+
+        // Closing scanner
+        input.close();
+    }
+}
