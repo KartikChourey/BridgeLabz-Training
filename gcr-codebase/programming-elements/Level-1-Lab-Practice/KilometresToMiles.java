3794480477e44781ca9c7dccf41c5683ed5a99c7
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-17 15:51:27 +0530

    [Kartik Chourey] : Added Program for Converting Kilometres to miles

---
+++ gcr-codebase/programming-elements/Level-1-Lab-Practice/KilometresToMiles.java
@@ -0,0 +1,13 @@
+public class KilometresToMiles {
+    public static void main(String[] args) {
+
+        // Distance in kilometers
+        double km = 10.8;
+
+        // Conversion factor
+        double miles = km / 1.6;
+
+        // Displaying output
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+}
