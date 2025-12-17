559bbf767d4dcc740973e9ceb4b5367ab76f452c
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-17 12:13:36 +0530

    [Kartik Chourey] : Added Program for Calculating Age

---
+++ gcr-codebase/programming-elements/Level-1-Lab-Practice/FindingAge.java
@@ -0,0 +1,15 @@
+public class FindingAge{
+public static void main (String args []){
+	// Harry's given birth year
+int birth_year = 2000;
+
+  // Given Current Year 
+int current_year =2024;
+
+// Calculating Harry's Present Age
+int harry_age = 2024-2000;
+
+// Displaying Harry's Present Age
+System.out.println(harry_age);
+}
+}
