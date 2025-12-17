2e6e77c6910bbb1b471d7e9985664c33f75b6eb3
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-17 15:30:13 +0530

    [Kartik Chourey] : Added Program for Calculating Average Marks

---
+++ gcr-codebase/programming-elements/Level-1-Lab-Practice/AverageMark.java
@@ -0,0 +1,15 @@
+public class AverageMark {
+    public static void main(String[] args) {
+
+        // Marks obtained in each subject
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        // Calculating average percentage
+        double average = (maths + physics + chemistry) / 3.0;
+
+        // Printing the result
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
