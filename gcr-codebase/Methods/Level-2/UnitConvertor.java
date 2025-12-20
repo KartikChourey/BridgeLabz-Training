11560fc8b79bde64bbd28a9ed4b57bf0173af51b
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-20 13:18:06 +0530

    [Kartik Chourey] : Added Program for Conversion of unit

---
+++ gcr-codebase/Methods/Level-2/UnitConvertor.java
@@ -0,0 +1,22 @@
+public class UnitConvertor{
+	
+	// Km to miles
+    public static double convertKmToMiles(double km){
+        return km*0.621371;
+    }
+	
+	// miles to km
+    public static double convertMilesToKm(double miles){
+        return miles*1.60934;
+    }
+	
+	// metres to feet
+    public static double convertMetersToFeet(double m){
+        return m*3.28084;
+    }
+	
+	// feet to meters
+    public static double convertFeetToMeters(double f){
+        return f*0.3048;
+    }
+}
