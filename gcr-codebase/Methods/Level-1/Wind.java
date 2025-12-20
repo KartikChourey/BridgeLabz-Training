e2132dd6464d1866c85059de6ad8ab92cbd20dad
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-20 11:48:02 +0530

    [Kartik Chourey] : Added Program for Wind Chill

---
+++ gcr-codebase/Methods/Level-1/Wind.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Wind{
+    // method to calculate wind chill
+    public static double calculateWindChill(double t,double v){
+        return 35.74+0.6215*t+(0.4275*t-35.75)*Math.pow(v,0.16);
+    }
+	
+	
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double t=sc.nextDouble();
+        double v=sc.nextDouble();
+		
+        // display wind chill
+        System.out.println("Wind Chill = "+calculateWindChill(t,v));
+       
+    }
+}
