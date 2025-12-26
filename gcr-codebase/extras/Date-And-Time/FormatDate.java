5666bd8bba54e85f85dc3628472f67c6de3c9181
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-26 10:09:09 +0530

    [Kartik Chourey] : Added the program for formattting date

---
+++ gcr-codebase/extras/Date-And-Time/FormatDate.java
@@ -0,0 +1,17 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class FormatDate{
+    public static void main(String[] args) {
+		LocalDate date = LocalDate.now();
+		DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd-MM-YYYY");
+		DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("YYYY0-MM-dd");
+		DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("EEE-YYYY-MM-dd");
+		
+		
+		// Display output 
+		System.out.println(date.format(formatter1));
+		System.out.println(date.format(formatter2));
+		System.out.println(date.format(formatter3));
+    }
+}
