883ae4677d12e288b064d056cbd69f70c607058a
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-27 00:19:34 +0530

    [Kartik Chourey] : Added Program for Pallindrome

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectandclass/level2/PallindromeChecker.java
@@ -0,0 +1,41 @@
+package com.objectandclass.level2;
+
+public class PallindromeChecker {
+	//Attribute
+	String text;
+	
+	//Creating the method check palindrome
+	PallindromeChecker(String text) {
+		this.text=text;
+		
+	}
+	//Creating the method to check its palindrome or not
+	public boolean isPalindrome() {
+	//To remove the space and return consistent text
+	String cleantext=text.replaceAll("\\s","").toLowerCase();
+	
+	//to reverse the string
+	String reverseText=new StringBuilder(cleantext).reverse().toString();
+	
+	return cleantext.equals(reverseText);
+	}
+	
+	public void diplayResult() {
+		if(isPalindrome()) {
+			System.out.println(text+" is palindrome");
+			}
+		else {
+			System.out.println(text+" is not Palindrome");
+		}
+	}
+	public static void main(String[] args) {
+		//creating object to check the text
+		 PallindromeChecker checker1 = new PallindromeChecker("A man a plan a canal Panama");
+		 checker1.diplayResult();
+		
+		 //creating object to check the text
+		 PallindromeChecker checker2 = new PallindromeChecker("Hello");
+		 checker2.diplayResult();
+	}
+}
+
