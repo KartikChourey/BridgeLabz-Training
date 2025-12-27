e652d66a47f12aa2f702a3c16ed73018f903e1cc
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-27 16:42:04 +0530

    [Kartik Chourey] : Added Program for Hotel Booking

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/HotelBooking.java
@@ -0,0 +1,26 @@
+package com.constructor.level1;
+
+public class HotelBooking {
+    String guestName;
+    String roomType;
+    int nights;
+
+    HotelBooking() {
+        guestName = "Guest";
+        roomType = "Standard";
+        nights = 1;
+    }
+
+    HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    // Copy constructor
+    HotelBooking(HotelBooking hb) {
+        this.guestName = hb.guestName;
+        this.roomType = hb.roomType;
+        this.nights = hb.nights;
+    }
+}
