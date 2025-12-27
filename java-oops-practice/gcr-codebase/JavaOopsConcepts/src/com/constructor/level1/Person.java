44fb125bba04c92a8f9c7e71d6d2d8e0cd980659
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-27 16:40:29 +0530

    [Kartik Chourey] : Added Program for person constructor

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Person.java
@@ -0,0 +1,18 @@
+package com.constructor.level1;
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Copy constructor
+    Person(Person p) {
+        this.name = p.name;
+        this.age = p.age;
+    }
+}
+
