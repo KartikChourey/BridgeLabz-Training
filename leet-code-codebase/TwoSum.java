0f8e9e124b98beeefd457df12f6562e0a598a27b
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-27 11:08:43 +0530

    [Kartik Chourey] : Added Program for leetcode #1 Two Sum

---
+++ leet-code-codebase/TwoSum.java
@@ -0,0 +1,22 @@
+
+// Leetcode #1
+// Two Sum 
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        int ans[]={-1};
+        for(int i=0;i<n;i++){
+            int partner = target-nums[i];
+            if(mp.containsKey(partner)){
+                ans= new int [] {i,mp.get(partner)};
+                return ans;
+            }
+            else{
+                mp.put(nums[i],i);
+            }
+
+        }
+        return ans;
+    }
+}
