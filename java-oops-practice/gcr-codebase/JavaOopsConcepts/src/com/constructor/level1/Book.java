f8c29d59e84f900f9602f25ccf7307f88ebca1c1
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-27 16:31:30 +0530

    [Kartik Chourey] : Added Program for constructor book class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Book.java
@@ -0,0 +1,26 @@
+package com.constructor.level1;
+
+class Book {
+    String title;
+    String author;
+    double price;
+
+    // Default constructor
+    Book() {
+        title = "Unknown";
+        author = "Unknown";
+        price = 0.0;
+    }
+
+    // Parameterized constructor
+    Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    void display() {
+        System.out.println(title + " | " + author + " | ₹" + price);
+    }
+}
+
