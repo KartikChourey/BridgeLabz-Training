8fa377717db1edd3471dcd5e572c6e15b8049945
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-30 16:20:28 +0530

    [Kartik Chourey] : Added program for Path Sum

---
+++ leet-code-codebase/PathSum.java
@@ -0,0 +1,10 @@
+// leetcode #112
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null) return false;
+       if(root!=null && root.left==null && root.right==null && targetSum==root.val)  return true; 
+       
+return  hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+    }
+}
