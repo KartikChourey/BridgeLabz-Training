848ce8e4ff7b89db54840e9e92cb37c043e85bca
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2025-12-31 16:25:17 +0530

    [Kartik Chourey] : Added program for animal hierarchy

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/animalhierarchy/Cat.java
@@ -0,0 +1,11 @@
+class Cat extends Animal {
+
+    Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " says: Meow Meow!");
+    }
+}
