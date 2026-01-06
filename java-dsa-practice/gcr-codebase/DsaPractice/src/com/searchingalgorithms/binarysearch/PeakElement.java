8ed03522efe67d00da72c5f50463c0a9cacc79ca
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2026-01-06 16:55:47 +0530

    [Kartik Chourey] : Added Program to find Peak element in an array

---
+++ java-dsa-practice/gcr-codebase/DsaPractice/src/com/searchingalgorithms/binarysearch/PeakElement.java
@@ -0,0 +1,22 @@
+package com.searchingalgorithms.binarysearch;
+
+public class PeakElement {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 20, 4, 1};
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        System.out.println("Peak Element: " + arr[left]);
+    }
+}
