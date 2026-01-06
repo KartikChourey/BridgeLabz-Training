8ab893d01459f22be205d2373ed00f208643dcf7
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2026-01-06 16:59:57 +0530

    [Kartik Chourey] : Added Program to find first and last occurance of a number

---
+++ java-dsa-practice/gcr-codebase/DsaPractice/src/com/searchingalgorithms/binarysearch/FirstLastOccurence.java
@@ -0,0 +1,35 @@
+package com.searchingalgorithms.binarysearch;
+
+public class FirstLastOccurence {
+
+    static int findFirst(int[] arr, int target) {
+        int left = 0, right = arr.length - 1, res = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                res = mid;
+                right = mid - 1;
+            } else if (arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return res;
+    }
+
+    static int findLast(int[] arr, int target) {
+        int left = 0, right = arr.length - 1, res = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                res = mid;
+                left = mid + 1;
+            } else if (arr[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,2,3,4};
+        System.out.println(findFirst(arr, 2) + ", " + findLast(arr, 2));
+    }
+}
