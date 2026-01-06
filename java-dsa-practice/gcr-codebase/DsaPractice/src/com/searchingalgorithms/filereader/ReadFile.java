7e4bfe56f740f90a2b5316aad0965a3e6b729e61
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2026-01-06 16:32:15 +0530

    [Kartik Chourey] : Added Program for Realfile line by line

---
+++ java-dsa-practice/gcr-codebase/DsaPractice/src/com/searchingalgorithms/filereader/ReadFile.java
@@ -0,0 +1,19 @@
+package com.searchingalgorithms.filereader;
+import java.io.*;
+
+public class ReadFile {
+
+    public static void main(String[] args) throws IOException {
+
+        FileReader fr = new FileReader("sample.txt");
+        BufferedReader br = new BufferedReader(fr);
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        br.close();
+        fr.close();
+    }
+}
