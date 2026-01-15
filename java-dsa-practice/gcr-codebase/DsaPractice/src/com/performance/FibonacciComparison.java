5918567ace388d7c717da942cad93b757e940d7c
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2026-01-15 11:35:40 +0530

    [Kartik Chourey] :  Added Program for performance check forFibonacci Series

---
+++ java-dsa-practice/gcr-codebase/DsaPractice/src/com/performance/FibonacciComparison.java
@@ -0,0 +1,26 @@
+package com.performance;
+
+public class FibonacciComparison {
+    static int fibRecursive(int n) {
+        if (n <= 1) return n;
+        return fibRecursive(n - 1) + fibRecursive(n - 2);
+    }
+
+    static int fibIterative(int n) {
+        int a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+
+        int n = 35;
+
+        BenchmarkUtils.measure("Recursive Fibonacci O(2^N)", () -> fibRecursive(n));
+        BenchmarkUtils.measure("Iterative Fibonacci O(N)", () -> fibIterative(n));
+    }
+}
