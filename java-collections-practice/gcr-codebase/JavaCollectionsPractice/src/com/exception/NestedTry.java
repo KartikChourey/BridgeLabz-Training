acbf47ca6af550a8887897fd86765b5a996e71b7
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2026-01-23 16:57:23 +0530

    [Kartik Chourey] : Added Program for NestedTry blocks

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/exception/NestedTry.java
@@ -0,0 +1,29 @@
+package com.exception;
+
+public class NestedTry {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30};
+        int index = 2;
+        int divisor = 0;
+
+        try {
+          
+            try {
+                int value = arr[index];
+
+              
+                System.out.println(value / divisor);
+
+            } catch (ArithmeticException e) {
+        
+                System.out.println("Cannot divide by zero!");
+            }
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+           
+            System.out.println("Invalid array index!");
+        }
+    }
+}
