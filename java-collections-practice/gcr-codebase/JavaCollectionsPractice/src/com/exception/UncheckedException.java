14d7daca142ab357780733735680f6e7c52b885c
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2026-01-23 14:46:31 +0530

    [Kartik Chourey] : Added Program for UnChecked Exception

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/exception/UncheckedException.java
@@ -0,0 +1,36 @@
+package com.exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class UncheckedException {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            // Taking first number
+            System.out.print("Enter first number: ");
+            int a = sc.nextInt();
+
+            // Taking second number
+            System.out.print("Enter second number: ");
+            int b = sc.nextInt();
+
+          
+            int result = a / b;
+            System.out.println("Result: " + result);
+
+        } catch (ArithmeticException e) {
+           
+            System.out.println("Cannot divide by zero");
+
+        } catch (InputMismatchException e) {
+          
+            System.out.println("Please enter only numbers");
+        }
+
+     
+    }
+}
