0043ec7a1abf6039d93bc1e5079706c67135ee87
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2026-01-26 23:55:33 +0530

    [Kartik Chourey] : Added Program for Exception testing

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/JUnit/exception/Divide.java
@@ -0,0 +1,11 @@
+package com.JUnit.exception;
+
+
+public class Divide {
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return a / b;
+    }
+}
