bc808a59a247fd3301229a01c1e0504c8592ca80
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2026-01-27 12:08:49 +0530

    [Kartik Chourey] : Added Program for Object Mapper

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/reflection/ObjectMapper.java
@@ -0,0 +1,25 @@
+package com.reflection;
+import java.lang.reflect.Field;
+import java.util.Map;
+
+class User {
+    String name;
+    int age;
+}
+
+public class ObjectMapper {
+
+    public static <T> T toObject(Class<T> clazz, Map<String, Object> map)
+            throws Exception {
+
+        T obj = clazz.getDeclaredConstructor().newInstance();
+
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+
+            Field field = clazz.getDeclaredField(entry.getKey());
+            field.setAccessible(true);
+            field.set(obj, entry.getValue());
+        }
+        return obj;
+    }
+}
