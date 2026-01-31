a83bd3de697989f2907bfcb4be485018117dadaa
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2026-01-31 16:05:29 +0530

    [Kartik Chourey] : Added Program for Email Notification

---
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/streamapi/Email.java
@@ -0,0 +1,17 @@
+package com.streamapi;
+
+import java.util.*;
+
+public class Email {
+
+    static void sendEmailNotification(String email) {
+        System.out.println("Email sent to " + email);
+    }
+
+    public static void main(String[] args) {
+
+        List<String> emails = List.of("a@gmail.com", "b@gmail.com");
+
+        emails.forEach(email -> sendEmailNotification(email));
+    }
+}
