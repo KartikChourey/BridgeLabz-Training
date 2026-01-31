f7d836d9706b2716b1b34a984824257c103d8f4a
KartikChourey <151906423+KartikChourey@users.noreply.github.com>
2026-01-31 14:45:09 +0530

    [Kartik Chourey] : Added Program for Payment System

---
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/functionalinterface/PaymentMain.java
@@ -0,0 +1,36 @@
+package com.functionalinterface;
+
+interface Payment {
+    void pay(double amount);
+}
+
+class UPI implements Payment {
+    public void pay(double amount) {
+        System.out.println("Paid ₹" + amount + " via UPI");
+    }
+}
+
+class CreditCard implements Payment {
+    public void pay(double amount) {
+        System.out.println("Paid ₹" + amount + " using Credit Card");
+    }
+}
+
+class Wallet implements Payment {
+    public void pay(double amount) {
+        System.out.println("Paid ₹" + amount + " through Wallet");
+    }
+}
+
+public class PaymentMain {
+    public static void main(String[] args) {
+
+        Payment p1 = new UPI();
+        Payment p2 = new CreditCard();
+        Payment p3 = new Wallet();
+
+        p1.pay(500);
+        p2.pay(1200);
+        p3.pay(300);
+    }
+}
